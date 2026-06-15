eb55bc1b0ac3e11ce7676763f47a54a39462a5ee
karomatulJamilia <karomatuljamilia99@gmail.com>
2026-06-15 13:01:33 +0700

    update

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,33 @@
-const form =
-document.getElementById("contactForm");
+// const form =
+// document.getElementById("contactForm");
+
+// form.addEventListener("submit", function(e){
+
+//     e.preventDefault();
+
+//     alert(
+//         "Pesan berhasil dikirim! Terima kasih telah menghubungi PalmWaste Hub."
+//     );
+
+//     form.reset();
+
+// });
+
+const form = document.getElementById("contactForm");
 
 form.addEventListener("submit", function(e){
 
     e.preventDefault();
 
+    const nama = document.getElementById("nama").value;
+    const email = document.getElementById("email").value;
+    const pesan = document.getElementById("pesan").value;
+
     alert(
-        "Pesan berhasil dikirim! Terima kasih telah menghubungi PalmWaste Hub."
+        "Pesan berhasil dikirim!\n\n" +
+        "Nama: " + nama + "\n" +
+        "Email: " + email + "\n" +
+        "Pesan: " + pesan
     );
 
     form.reset();
